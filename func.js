062af91cd315ed06daa1bf8186f567e0b45e81cf
THARANIPRAKASH <tharaniprakashoff07@gmail.com>
2025-07-28 22:37:34 +0530

    day1 commit

---
+++ func.js
@@ -0,0 +1,45 @@
+// 
+
+
+
+//const greet = (name) => `Hello, ${name}`;
+//greet("hi");
+
+// Array
+//const [first, second] = [1, 2, 3];
+
+// Object
+//const { name, age } = { name: "John", age: 25 };
+
+// // Spread
+// const arr1 = [1, 2];
+// const arr2 = [...arr1, 3]; // [1, 2, 3]
+
+// // Rest
+// function sum(...nums) {
+//   return nums.reduce((a, b) => a + b, 0);
+// }
+
+// const obj = {
+//   name: "React",
+//   getName: () => {
+//     return this.name;
+//   }
+// };
+// console.log(obj.getName());
+
+
+
+
+
+
+
+// const person = {
+//   age: 30,
+//   growOld: function() {
+//     setInterval(() => {
+//       this.age++;
+//       console.log(this.age);
+//     }, 1000);
+//   }
+// };
