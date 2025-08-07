3c0b923a789a3c778b899689c8945644fd62665b
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-08-07 07:46:28 +0530

    LifeCycle commit

---
+++ day3/src/intermediate/LifeCycle.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const LifeCycle = () => {
+
+    const [count, setCount] = useState(0)
+
+    //mount
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         console.log("Timer running...");
+    //     }, 1000);
+    // },[])
+    
+    
+    //update
+
+    // useEffect(() => {
+    //     console.log('Component mounted or updated');
+    // }, [count]);
+
+
+    //unmount
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            console.log("Running...");
+        }, 1000);
+
+        return () => {
+            clearInterval(timer); 
+            console.log("Timer cleared on unmount");
+        };
+    }, []);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LifeCycle
