ff33457308600de612bae4f37e41f8c7b082aa24
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-08-26 06:27:40 +0530

    create filtering with search at client side

--- day3/src/advanced/Filter.jsx
+++ day3/src/advanced/Filter.jsx
@@ -1,1 +1,29 @@
-//filter
+import {useState} from "react";
+
+const Filter = () => {
+    const data =["Apple", "Banana", "Orange", "Mango", "Pineapple"];
+    const [search,setSearch] = useState();
+
+    const filteredData = data.filter((item) => 
+        item.toLowerCase().includes(search.toLowerCase())
+    );
+  return (
+    <div>
+        <h2>Filter Example</h2>
+        <input
+        type="text"
+        placeholder="Search fruits..."
+        onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <ul>
+            {filteredData.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Filter
