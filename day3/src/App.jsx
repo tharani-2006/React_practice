d7129eb01066827e9d7b923e0c30be2da24673ac
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-08-28 06:31:49 +0530

    create routes setup app

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,125 +1,152 @@
-//import logo from './logo.svg';
-import './App.css';
-//import UseMemoExample from './intermediate/UseMeno.jsx';
-//import TimerApp from './intermediate/TimerApp';
-// import Demo from './fundamentals/Demo.jsx'; 
-// import Func_comp from './fundamentals/Func_comp.jsx';
-// import ProductDisplay from './ProductDisplay .jsx';
-// import Props_demo from './Props_demo.jsx';
-// import Button from './fundamentals/Button.jsx';
-// import Counter from './fundamentals/Counter.jsx';
-// import Handling from './fundamentals/Handling.jsx';
-// import ConditionalRendering from './fundamentals/ConditionalRendering.jsx';
-// import Toggle from './Toggle.jsx';
-// import Map from './Map.jsx';
-// import CounterApp from './fundamentals/CounterPrj.jsx';
-// import TodoApp from './ToDo.jsx';
-// import CalculatorApp from './fundamentals/Calculator.jsx';
-//import UseEffect from './intermediate/UseEffect';
-// import LifeCycle from './intermediate/LifeCycle.jsx';
-// import Home from './intermediate/Home.jsx';
-// import {BrowserRouter , Routes,Route,Link} from 'react-router-dom';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import CreatePost from './intermediate/Blog/CreatePost.jsx';
-// import PostDetail from './intermediate/Blog/PostDetail.jsx';
-// import Navbar from './intermediate/Blog/components/Navbar.jsx';
-// import Home from './intermediate/Blog/Home.jsx';
-// // import { useState } from 'react';
-// import UseRef from './intermediate/UseRef.jsx';
-// import UseRefExample from './intermediate/UseRefExample.jsx';
-// import UseMeno from './intermediate/UseMeno.jsx';
-//import UseCallbackExample from './intermediate/UseCallback.jsx';
-// import Form from './intermediate/Form.jsx';
-//import Parent from './intermediate/LiftingUp/Parent.jsx';
-//import UncontrolledInputs from './intermediate/UncontrolledInputs.jsx';
-//import ControlledInputs from './intermediate/ControlledInputs.jsx';
-// import { UserProvider } from './intermediate/useContext/CreateContext.jsx';
-// import Profile from './intermediate/useContext/UpdateContext.jsx';
-// import Home from './intermediate/useContext/UseContext.jsx';
-// //import UseContext from './intermediate/useContext/UseContext.jsx';
-// import { ThemeProvider } from './intermediate/ContextAPI/CreateContext';
-// import Theme from './intermediate/ContextAPI/Theme.jsx';
-import Fetch from './advanced/Fetch.jsx';
-import FetchPost from './advanced/FetchPost.jsx';
+// //import logo from './logo.svg';
+// import './App.css';
+// //import UseMemoExample from './intermediate/UseMeno.jsx';
+// //import TimerApp from './intermediate/TimerApp';
+// // import Demo from './fundamentals/Demo.jsx'; 
+// // import Func_comp from './fundamentals/Func_comp.jsx';
+// // import ProductDisplay from './ProductDisplay .jsx';
+// // import Props_demo from './Props_demo.jsx';
+// // import Button from './fundamentals/Button.jsx';
+// // import Counter from './fundamentals/Counter.jsx';
+// // import Handling from './fundamentals/Handling.jsx';
+// // import ConditionalRendering from './fundamentals/ConditionalRendering.jsx';
+// // import Toggle from './Toggle.jsx';
+// // import Map from './Map.jsx';
+// // import CounterApp from './fundamentals/CounterPrj.jsx';
+// // import TodoApp from './ToDo.jsx';
+// // import CalculatorApp from './fundamentals/Calculator.jsx';
+// //import UseEffect from './intermediate/UseEffect';
+// // import LifeCycle from './intermediate/LifeCycle.jsx';
+// // import Home from './intermediate/Home.jsx';
+// // import {BrowserRouter , Routes,Route,Link} from 'react-router-dom';
+// // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// // import CreatePost from './intermediate/Blog/CreatePost.jsx';
+// // import PostDetail from './intermediate/Blog/PostDetail.jsx';
+// // import Navbar from './intermediate/Blog/components/Navbar.jsx';
+// // import Home from './intermediate/Blog/Home.jsx';
+// // // import { useState } from 'react';
+// // import UseRef from './intermediate/UseRef.jsx';
+// // import UseRefExample from './intermediate/UseRefExample.jsx';
+// // import UseMeno from './intermediate/UseMeno.jsx';
+// //import UseCallbackExample from './intermediate/UseCallback.jsx';
+// // import Form from './intermediate/Form.jsx';
+// //import Parent from './intermediate/LiftingUp/Parent.jsx';
+// //import UncontrolledInputs from './intermediate/UncontrolledInputs.jsx';
+// //import ControlledInputs from './intermediate/ControlledInputs.jsx';
+// // import { UserProvider } from './intermediate/useContext/CreateContext.jsx';
+// // import Profile from './intermediate/useContext/UpdateContext.jsx';
+// // import Home from './intermediate/useContext/UseContext.jsx';
+// // //import UseContext from './intermediate/useContext/UseContext.jsx';
+// // import { ThemeProvider } from './intermediate/ContextAPI/CreateContext';
+// // import Theme from './intermediate/ContextAPI/Theme.jsx';
+// import Fetch from './advanced/Fetch.jsx';
+// import FetchPost from './advanced/FetchPost.jsx';
+// function App() {
+
+
+//   //   const [posts, setPosts] = useState([
+//   //     { id: 1, title: "First Post", content: "This is my first blog post" },
+//   //     { id: 2, title: "Second Post", content: "This is my second blog post" }
+//   // ]);
+//   return (
+//     <div className="App">
+//       <h1>users</h1>
+      
+//       <FetchPost />
+//       <Fetch />
+
+//     {/* <ThemeProvider>
+//       <Theme />
+//     </ThemeProvider> */}
+
+
+
+
+
+
+
+      
+//       {/* <UseCallbackExample /> */}
+//           {/* <UseMeno /> */}
+//       {/* <UseRef/>
+//       <UseRefExample /> */}
+// {/*  <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home posts={posts} />} />
+//         <Route path="/post/:id" element={<PostDetail posts={posts} />} />
+//         <Route path="/create" element={<CreatePost setPosts={setPosts} />} />
+//       </Routes>
+//     </Router> */}
+//       {/* <Demo />
+//       <Func_comp name= " thara" isLoggedIn="true"/>
+//       <ProductDisplay/>
+//       <Props_demo name="tharani" length={10}/>
+//       <Props_demo name="madam" length={20}/>
+//       <Props_demo name="Sister" length={30}/>
+
+//       <Button label="Click Me" onClick={() => alert('Button Clicked!')} />
+//       <Button label="Submit" onClick={() => alert('Form Submitted!')} />
+//       <Button label="Tharani" onClick={() => alert('Tharani go ahead')} /> 
+//       <Counter />
+//       <Handling />
+//       <ConditionalRendering/>
+//       <Toggle />
+//       <Map />*/}
+      
+//       {/* <CounterApp />
+//       <TodoApp />
+//       <CalculatorApp/> 
+//       <UseEffect />*/}
+      
+//       {/* <TimerApp /> */}
+
+
+//       {/* <UncontrolledInputs /> */}/
+//       {/* <Form /> */}
+//       {/* <BrowserRouter>
+//         <nav>
+//           <Link to="/">Home</Link>
+//           <Link to="/lifecycle">Life Cycle</Link>
+//           <Link to="/timer">Timer App</Link>
+//         </nav>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/lifecycle" element={<LifeCycle />} />
+//           <Route path="/timer" element={<TimerApp />} />
+//         </Routes>
+//       </BrowserRouter> 
+//       <Parent />
+// */}
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import NotFound from "./pages/NotFound";
+import "./App.css";
+
 function App() {
-
-
-  //   const [posts, setPosts] = useState([
-  //     { id: 1, title: "First Post", content: "This is my first blog post" },
-  //     { id: 2, title: "Second Post", content: "This is my second blog post" }
-  // ]);
   return (
-    <div className="App">
-      <h1>users</h1>
-      
-      <FetchPost />
-      <Fetch />
-
-    {/* <ThemeProvider>
-      <Theme />
-    </ThemeProvider> */}
-
-
-
-
-
-
-
-      
-      {/* <UseCallbackExample /> */}
-          {/* <UseMeno /> */}
-      {/* <UseRef/>
-      <UseRefExample /> */}
-{/*  <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home posts={posts} />} />
-        <Route path="/post/:id" element={<PostDetail posts={posts} />} />
-        <Route path="/create" element={<CreatePost setPosts={setPosts} />} />
-      </Routes>
-    </Router> */}
-      {/* <Demo />
-      <Func_comp name= " thara" isLoggedIn="true"/>
-      <ProductDisplay/>
-      <Props_demo name="tharani" length={10}/>
-      <Props_demo name="madam" length={20}/>
-      <Props_demo name="Sister" length={30}/>
-
-      <Button label="Click Me" onClick={() => alert('Button Clicked!')} />
-      <Button label="Submit" onClick={() => alert('Form Submitted!')} />
-      <Button label="Tharani" onClick={() => alert('Tharani go ahead')} /> 
-      <Counter />
-      <Handling />
-      <ConditionalRendering/>
-      <Toggle />
-      <Map />*/}
-      
-      {/* <CounterApp />
-      <TodoApp />
-      <CalculatorApp/> 
-      <UseEffect />*/}
-      
-      {/* <TimerApp /> */}
-
-
-      {/* <UncontrolledInputs /> */}/
-      {/* <Form /> */}
-      {/* <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/lifecycle">Life Cycle</Link>
-          <Link to="/timer">Timer App</Link>
-        </nav>
+    <Router>
+      <Header />
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/lifecycle" element={<LifeCycle />} />
-          <Route path="/timer" element={<TimerApp />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter> 
-      <Parent />
-*/}
-      
-    </div>
+      </div>
+    </Router>
   );
 }
 
